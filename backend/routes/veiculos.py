cce92b5ef14555c60626a024d764f224a536c4d6
Pedro Serrano <163578377+PedroSerrano29@users.noreply.github.com>
2026-08-05 06:12:39 +0000

    Atualizar veiculos.py

--- backend/routes/veiculos.py
+++ backend/routes/veiculos.py
@@ -1,7 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from models import Veiculo
 
 veiculos_bp = Blueprint('veiculos', __name__)
 
-@veiculos_bp.route('api/veiculos', methods=['GET'])
+@veiculos_bp.route('/api/veiculos', methods=['GET'])
 def listar_veiculos():
-    return {"mensagem": "ainda vazio"}
+    veiculos = Veiculo.query.all()
+    resultado = [
+        {
+            "id": v.id,
+            "marca": v.marca,
+            "modelo": v.modelo,
+            "categoria": v.categoria,
+            "transmissao": v.transmissao,
+            "tipo": v.tipo,
+            "capacidade_pessoas": v.capacidade_pessoas,
+            "valor_diaria": v.valor_diaria,
+            "imagem_url": v.imagem_url,
+            "disponivel": v.disponivel
+        }
+        for v in veiculos
+    ]
+    return jsonify(resultado)
