db17fc608a60d0bce7c37acde9cb0045c979ee0f
Pedro Serrano <163578377+PedroSerrano29@users.noreply.github.com>
2026-08-05 05:45:02 +0000

    Criar app.py

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from config import Config
+from models import db
+from routes.veiculos import veiculos_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    app.register_blueprint(veiculos_bp)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
